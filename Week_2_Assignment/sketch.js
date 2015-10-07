b0e18ff13e846f5c0a8e6e1d78c20443f39f75d5
DarkWizard <nayeemaquib@bennington.edu>
2015-10-07 11:22:53 -0400

    Adding Week 2 Assignment

---
+++ Week_2_Assignment/sketch.js
@@ -0,0 +1,53 @@
+var num_x;
+var num_y;
+var size;
+
+function setup() {
+  createCanvas(800, 800);
+  rectMode(CENTER);
+  
+  // noLoop();
+
+  // create and call our own function
+  setVariables();
+}
+
+// you can create your own function.
+// let's make one which initializes all of the global
+// variables.
+function setVariables() {
+  num_x = random(3, 24);
+  num_y = random(3, 24);
+  size = random(50, 100);
+}
+
+function draw() {
+  background(0);
+
+  for (var i=0; i<num_x; i++) {
+    for (var j=0; j<num_y; j++) {
+      
+      var x = map(i, 0, num_x, 0, width);
+      var y = map(j, 0, num_y, 0, height);
+      
+      var offset_x = map(sin(0.01 * i * frameCount), -1, 1, -20, 20);
+      var mult_y = map(sin(0.001 * i * j * frameCount), -1, 1, 0.5, 2.0);
+      
+      var opacity = map(sin(0.001 * i * j * frameCount), -1, 1, 0, 150);
+      
+      // Assignment Part
+      
+      fill(x, y, offset_x, opacity); // coloring accordingly
+      rect(x + offset_x, y, mouseX, mouseY, 20); // mouse interaction
+    }
+  }  
+  setVariables(); // changing shape everyframe instead of 'pressing spacebar'
+}
+
+/*
+function keyPressed() {
+  if (key==' ') {
+    setVariables();
+  }
+}
+*/
