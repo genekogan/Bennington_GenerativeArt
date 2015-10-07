3ddb0449485beac16fa718527b94a2c513b7d0b2
DarkWizard <nayeemaquib@bennington.edu>
2015-10-07 13:34:04 -0400

    Adding week 3 assignment

---
+++ Week_3_Assignment/assignment.md
@@ -0,0 +1,9 @@
+### Assignment week 3
+
+
+There are two simple assignments this week.
+
+1) Look at the final example, p19_perlin_noisyBeziers. Instead of controlling the coordinates using perlin noise, try to switch some or all of the coordinates to be controlled with sine curves instead. Try making bezier curves whose coordinates oscillate along the screen using sine curves, or perhaps a combination where some coordinates are fixed, some are moving with perlin noise, and some are moving with sine curves.
+
+2) Look at the example, p18_perlin_noisyTriangles_2.  Add color variation to it according to some strategy. If you wish, you can also try to do the suggested improvements from assignment #1 but for the triangles as well.
+
---
+++ Week_3_Assignment/p18_perlin_noisyTriangles_2/sketch.js
@@ -0,0 +1,48 @@
+var nx1;
+var nx2;
+var nx3;
+var ny1;
+var ny2;
+var ny3;
+
+function setup() {
+  createCanvas(1650, 1080);
+  background(255);
+  
+  // let all the initial noise sampling
+  // variables be different, so our samples
+  // aren't all initially the same
+  nx1 = 10;
+  nx2 = 20;
+  nx3 = 30;
+  ny1 = 40;
+  ny2 = 50;
+  ny3 = 60;
+}
+
+function draw() {
+  
+  var x1 = width * noise(nx1);
+  var x2 = width * noise(nx2);
+  var x3 = width * noise(nx3);
+  var y1 = height * noise(ny1);
+  var y2 = height * noise(ny2);
+  var y3 = height * noise(ny3);
+
+  var r = random(random(0, 255),random(0, 255));
+  var g = random(random(0, 255),random(0, 255));
+  var b = random(random(0, 255),random(0, 255));
+  
+  noStroke();
+  fill(r, g, b, 20);
+  triangle(x1, y1, x2, y2, x3, y3);
+
+  // change the noise samples
+  nx1 += 0.0011;
+  nx2 += 0.0022;
+  nx3 += 0.0017;
+  ny1 += 0.0013;
+  ny2 += 0.0019;
+  ny3 += 0.0015;
+  
+}
